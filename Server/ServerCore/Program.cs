0552314e0e7002936c5e1dbd6754608f62b2afe3
NaWoongTae <46421475+NaWoongTae@users.noreply.github.com>
2021-09-03 02:06:51 +0900

    server
    context switching

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -19,8 +19,12 @@ namespace ServerCore
                 // CAS Compare-And-Swap
                 int expected = 0; // 예상값;
                 int desired = 1; // 원하는값
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected) // 
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected) // 여기서 즉시 재시도 해야 spinlock
                     break;
+
+                Thread.Sleep(1); // 1ms 쉼
+                Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인
+                Thread.Yield(); // 관대한 양보 => 지금 실행 가능한 쓰레드가 있으면 실행가능 => 실행 가능한 애 없으면 다시 본인
             }
         }
 
