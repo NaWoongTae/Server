6add3b09839a637e617c6ebffd5b32fe27b8038a
NaWoongTae <46421475+NaWoongTae@users.noreply.github.com>
2021-09-03 02:54:02 +0900

    server - Multi Thread Programming
    lock - ReadWriteLock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,39 +7,30 @@ namespace ServerCore
 {
     class Program
     {
-        // 근성 spinlock
-        // 양보 sleep, yield
-        // 갑질 AutoResetEvent
+        // 특정 순간에만 lock
+        // writeLock이 걸리지 않는다면 ReadLock이 없는것처럼 작동한다.
+        static ReaderWriterLockSlim _lock4 = new ReaderWriterLockSlim();
 
-        static object _lock = new object();
+        class Reward { }
+        static Reward GetRewardById(int id)
+        {
+            _lock4.EnterReadLock();
+            // 행동
+            _lock4.ExitReadLock();
 
-        // 사실 내부적으로 구현되어있음
-        // 근성으로 버티지만 너무 오랜시간 대기할경우 간헐적으로 yield를 시행한다.
-        static SpinLock _lock2 = new SpinLock();
-        
-        // 오래 걸림
-        // 별도의 프로그램끼리도 동기화가 가능함
-        static Mutex _lock3 = new Mutex();
+            return null;
+        }
+
+        void AddReward(Reward reward)
+        {
+            _lock4.EnterWriteLock();
+
+            _lock4.ExitWriteLock();
+        }
 
         static void Main(string[] args)
         {
-            // ex) _lock
-            lock (_lock) // 내부적으로는 Monitor를 사용함
-            {
-                
-            }
 
-            // ex) _lock2
-            bool lockTaken = false;
-            try
-            {
-                _lock2.Enter(ref lockTaken);
-            }
-            finally 
-            {
-                if (lockTaken)
-                    _lock2.Exit();
-            }
         }
     }
 }
