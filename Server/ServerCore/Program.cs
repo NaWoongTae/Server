73a41262237f5c9a4899a3ba94cf7d34ea9f208f
NaWoongTae <46421475+NaWoongTae@users.noreply.github.com>
2021-09-03 02:48:26 +0900

    server - multi thread programming
    기본 lock 정리

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,44 +7,39 @@ namespace ServerCore
 {
     class Program
     {
-        static int _num = 0;
+        // 근성 spinlock
+        // 양보 sleep, yield
+        // 갑질 AutoResetEvent
 
-        // AutoResetEvent보다 많은 데이터를 가짐
-        // lock 횟수 저장
-        // 쓰레드 ID - 잘못된 애가 릴리즈 하지않도록
-        // 때문에 코스트가 좀더 듬
-        static Mutex _lock = new Mutex(); // spinlock보다는 느림 -> 커널동기화 객체
+        static object _lock = new object();
 
-        static void Thread_1()
-        {
-            foreach (int i in Enumerable.Range(1, 100000))
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            foreach (int i in Enumerable.Range(1, 100000))
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
+        // 사실 내부적으로 구현되어있음
+        // 근성으로 버티지만 너무 오랜시간 대기할경우 간헐적으로 yield를 시행한다.
+        static SpinLock _lock2 = new SpinLock();
+        
+        // 오래 걸림
+        // 별도의 프로그램끼리도 동기화가 가능함
+        static Mutex _lock3 = new Mutex();
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
+            // ex) _lock
+            lock (_lock) // 내부적으로는 Monitor를 사용함
+            {
+                
+            }
 
-            Task.WaitAll(t1, t2);
-
-            Console.Write(_num);
+            // ex) _lock2
+            bool lockTaken = false;
+            try
+            {
+                _lock2.Enter(ref lockTaken);
+            }
+            finally 
+            {
+                if (lockTaken)
+                    _lock2.Exit();
+            }
         }
     }
 }
