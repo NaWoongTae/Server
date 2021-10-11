8a0b2778a5663943f9f44d05ace383898f9dc4cf
NaWoongTae <46421475+NaWoongTae@users.noreply.github.com>
2021-10-12 05:35:58 +0900

    SQL TUNING
    SCAN SEEK

--- SQLQueryClustered.sql
+++ SQLQueryClustered.sql
@@ -36,6 +36,8 @@ FROM sys.indexes
 WHERE object_id = object_id('TestOrderDetails');
 
 -- 조회
+DBCC IND('Northwind', 'TestOrderDetails', 0);
+DBCC IND('Northwind', 'TestOrderDetails', 1);
 DBCC IND('Northwind', 'TestOrderDetails', 2);
 
 -- Heap RID ([페이지 주소(4)][파일ID(2)][슬ㄹ롯(2)] Row)
@@ -50,3 +52,84 @@ DBCC PAGE('Northwind', 1, 1016, 3);
 
 -- 조회
 DBCC IND('Northwind', 'TestOrderDetails', 1); -- root 968
+DBCC PAGE('Northwind', 1, 968, 3);
+
+DBCC PAGE('Northwind', 1, 930, 3);
+
+-- ===================================================================================
+-- ===================================================================================
+
+-- 인덱스 접근 방식 (Access)
+-- Index Scan vs Index Seek
+
+CREATE TABLE TestAccess
+(
+	id INT NOT NULL,
+	name NCHAR(50) NOT NULL,
+	dummy NCHAR(1000) NOT NULL
+);
+GO
+
+CREATE CLUSTERED INDEX TestAccess_CI
+ON TestAccess(id);
+GO
+
+CREATE NONCLUSTERED INDEX TestAccess_NCI
+ON TestAccess(name);
+GO
+
+DECLARE @i INT;
+SET @i = 1;
+
+WHILE (@i <= 500)
+BEGIN
+	INSERT INTO TestAccess
+	VALUES (@i, 'Name' + CONVERT(VARCHAR, @i), 'Hello World' + CONVERT(VARCHAR, @i * @i));
+	SET @i = @i + 1;
+END
+
+-- 테이블
+SELECT * FROM TestAccess;
+
+-- 인덱스 정보
+EXEC sp_helpindex 'TestAccess';
+
+-- 인덱스 번호
+SELECT index_id, name
+FROM sys.indexes
+WHERE object_id = object_id('TestAccess');
+
+-- 조회
+DBCC IND ('Northwind', 'TestAccess', 1);
+DBCC IND ('Northwind', 'TestAccess', 2);
+
+-- CLUSTERED(1) : id
+-- 857
+-- 856 ~ 1127
+
+-- NON CLUSTERED(2) : name
+-- 865
+-- 864 ~ 1061
+
+-- 논리적 읽기 -> 실제 데이터를 찾기 위해 읽은 페이지수
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+-- INDEX SCAN - 전체 167번
+SELECT * FROM TestAccess;
+
+-- INDDEX SEEK
+-- 클러스터드 조회 -> 루트(1) + 리프(1) = 논리적 읽기 2번
+SELECT * FROM TestAccess
+WHERE id = 104;
+
+-- INDDEX SEEK + KEY LOOKUP
+-- 논 클러스터드 조회 -> Data?Index? Page 루트(1) + 리프(1) => 클러스터드 키 추출 => 클러스터드 조회 -> 루트(1) + 리프(1) = 논리적 읽기 4번
+SELECT * FROM TestAccess
+WHERE name = 'name104';
+
+-- INDDEX SCAN + KEY LOOKUP
+-- ORDER BY + TOP 으로 논리적 읽기 횟수 감소
+SELECT TOP 5 * 
+FROM TestAccess
+ORDER BY name;
--- etc.sql
+++ etc.sql
@@ -2,3 +2,77 @@ USE GameDB;
 
 SELECT *
 FROM accounts;
+
+DROP INDEX accounts.i1;
+
+ALTER TABLE accounts
+DROP CONSTRAINT PK_ACC;
+
+--CREATE UNIQUE CLUSTERED INDEX PK_ACC PRIMARY KEY(accountId);
+
+ALTER TABLE accounts
+ADD CONSTRAINT PK_2 PRIMARY KEY (accountName);
+
+CREATE UNIQUE CLUSTERED INDEX PK_ACC ON accounts(accountId);
+CREATE CLUSTERED INDEX PK_ACC ON accounts(accountId);
+DROP INDEX accounts.PK_ACC;
+
+EXEC sp_helpindex 'accounts';
+
+-- PRIMARY KEY += UNIQUE
+
+CREATE INDEX PK_3 ON accounts(accountId);
+CREATE INDEX PK_ac2 ON accounts(accountName);
+
+SELECT * FROM sys.indexes WHERE object_id = object_id('accounts');
+SELECT * FROM sys.indexes WHERE type_desc = 'HEAP';
+SELECT * FROM sys.indexes WHERE NAME IS NULL;
+
+SELECT index_id, name
+FROM sys.indexes
+WHERE object_id = object_id('accounts');
+
+SELECT object_id('accounts');
+
+DBCC IND('GameDB', 'accounts', 0); -- 0이랑 1이랑 결과 똑같음 / 왜지
+DBCC IND('GameDB', 'accounts', 1); -- 89 280 // 앞의 숫자는 머임
+DBCC IND('GameDB', 'accounts', 2); -- 118 432
+DBCC IND('GameDB', 'accounts', 3); -- 119 440
+DBCC IND('GameDB', 'accounts', 4); -- 120 448
+
+SELECT * FROM sys.indexes WHERE object_id = object_id('accounts');
+DBCC PAGE('GameDB', 1, 280, 3);
+DBCC PAGE('GameDB', 1, 118, 3);
+DBCC PAGE('GameDB', 1, 440, 3);
+
+-- UNIQUE / PRIMARY KEY / 
+
+SELECT *
+INTO acc2
+FROM [accounts];
+
+SELECT * FROM acc2;
+
+CREATE INDEX Ind1 ON acc2(accountId);
+CREATE INDEX Ind2 ON acc2(accountName);
+CREATE CLUSTERED INDEX cInd1 ON acc2(accountId);
+ALTER TABLE acc2
+ADD CONSTRAINT ac2Key PRIMARY KEY(accountId);
+DROP INDEX acc2.cInd1;
+
+EXEC sp_helpindex 'acc2';
+
+SELECT * FROM sys.indexes WHERE object_id = object_id('acc2'); -- index_id (0 : Heap) (1 : CLUSTERED) (2~ : NON-CLUSTERED)
+
+DBCC IND('GameDB', 'acc2', 0); -- 142 456
+DBCC IND('GameDB', 'acc2', 1); -- 142 456
+DBCC IND('GameDB', 'acc2', 2); -- 146 288 - index_id가 page type?
+
+-- 145 456
+-- 150 464
+
+DBCC PAGE('GameDB', 1, 145, 3);
+DBCC PAGE('GameDB', 1, 456, 3);
+DBCC PAGE('GameDB', 1, 150, 3);
+DBCC PAGE('GameDB', 1, 464, 3);
+
