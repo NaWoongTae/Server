e77f570fa3e0a3319ebcb42d05a2405879eb50e0
NaWoongTae <46421475+NaWoongTae@users.noreply.github.com>
2021-10-29 04:47:36 +0900

    ASP.NET Core
    MVC 개론

---
+++ ASPcore/HelloAspNet/HelloEmpty/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloEmpty.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloMessage msg = new HelloMessage()
+            {
+                Message = "Welcome to Jurasic World!!"
+            };
+
+            // dynamic type
+            ViewBag.Noti = "Input message and click submit";
+
+            return View(msg);
+        }
+
+        // POST를 처리하는
+        [HttpPost]
+        public IActionResult Index(HelloMessage obj)
+        {
+            ViewBag.Noti = "Message Changed";
+            return View(obj);
+        }
+    }
+}
